c92385a929f9e4bee60545c2d0fee96ccf9c1074
Ubuntu <jhhernan@TopV5.gr41ysqeo4pujnzjf3zvbwcfbc.bx.internal.cloudapp.net>
2020-08-29 22:54:31 +0000

    Shows google map

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const mapStyles = {
+  width: '60%',
+  height: '60%',
+};
+
+class App extends React.Component<any>{
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Map  
+            google={this.props.google}     
+            zoom={12}
+            style={mapStyles}
+            initialCenter={
+              { lng: -74.06725643633541, lat: 4.8293179395713 }
+            }
+          >
+          </Map>
+
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+//export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyAePvgmHTkh7fyL1UdIAv-uEmS4snQ3Mz4'
+})(App);
